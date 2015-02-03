8773e59fd0b623addfe4d2b24c2ef340cdc8b900
cdbajorin <christopher.bajorin@gmail.com>
2015-02-03 15:49:33 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,116 @@
+'use strict';
+
+/**
+ *
+ * @param {Number|String} height
+ * @param {String} heightUnit
+ * @param {Number|String} weight
+ * @param {String} weightUnit
+ * @returns {Number|Error}
+ */
+
+exports.getBMI = function(height, heightUnit, weight, weightUnit) {
+
+    if (arguments.length !== 4) {
+        return new Error("Function requires 4 inputs: height, height unit, weight, weight unit");
+    }
+
+    height = parseFloat(height);
+    heightUnit = heightUnit.toLowerCase();
+    weight = parseFloat(weight);
+    weightUnit = weightUnit.toLowerCase();
+
+    // validate height and weight
+    try {
+        validateValues(height, weight);
+    } catch (error) {
+        return error
+    }
+
+    try {
+        var unitType = getUnitType(heightUnit, weightUnit);
+    } catch(error) {
+        return error
+    }
+    return calculateBMI(height, weight, unitType);
+};
+
+/**
+ * Checks if the height and weight values are valid
+ *
+ * @param height
+ * @param weight
+ * @returns {Error}
+ */
+
+function validateValues (height, weight) {
+
+    if (!height) {
+        throw new Error("Invalid height");
+    }
+    if (!weight) {
+        throw new Error("Invalid weight");
+    }
+}
+
+/**
+ * Gets the unit type used in BMI calculation
+ *
+ * @param {String} heightUnit
+ * @param {String} weightUnit
+ * @returns {String|Error}
+ */
+
+function getUnitType(heightUnit, weightUnit) {
+
+    var imperialHeight = false,
+        metricHeight = false,
+        imperialWeight = false,
+        metricWeight = false;
+
+    if ( heightUnit === "inches" || heightUnit === "feet" ) {
+        imperialHeight = true;
+    } else if ( heightUnit === "meters" || heightUnit === "m" ) {
+        metricHeight = true;
+    }
+
+    if ( weightUnit === "pounds" || weightUnit === "lbs" ) {
+        imperialWeight = true;
+    } else if ( weightUnit === "kilograms" || weightUnit === "kg" ) {
+        metricWeight = true;
+    }
+
+    if (metricHeight && metricWeight) {
+        return "metric";
+    } else if (imperialHeight && imperialWeight) {
+        return "imperial";
+    } else {
+        throw new Error("Invalid Unit Types\n\n" +
+        "Acceptable unit types:\n" +
+        "Height: 'meters', 'm', 'inches' \n" +
+        "Weight: 'kilograms', 'kg', 'pounds', 'lbs'\n\n");
+    }
+}
+
+/**
+ * Calculates BMI based on the measuring system
+ *
+ * @param {Number} height
+ * @param {Number} weight
+ * @param {String} type
+ * @returns {Number|Error}
+ */
+
+function calculateBMI(height, weight, type) {
+
+    var bmi = weight / (height * height);
+
+    if (type === "imperial") {
+        return Math.round( bmi * 703 * 100 ) / 100;
+    }
+    if (type === "metric") {
+        return Math.round( bmi * 100) / 100;
+    } else {
+        return new Error("Unable to calculate BMI");
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,86 @@
+"use strict";
+
+var chai = require("chai");
+var bmi = require("./../index");
+
+var expect = chai.expect;
+
+describe("Calculate BMI", function() {
+
+    it("Correctly calculates imperial bmi with numbers", function(done) {
+
+        var height = 72;
+        var weight = 150;
+        var hUnit = "inches";
+        var wUnit = "pounds";
+        var value = bmi.getBMI(height, hUnit, weight, wUnit);
+
+        expect(value).to.not.be.an.instanceof(Error);
+        expect(value).to.equal(20.34);
+        done();
+    });
+
+    it("Correctly calculates metric bmi with numbers", function(done) {
+
+        var height = 1.8;
+        var weight = 68;
+        var hUnit = "meters";
+        var wUnit = "kilograms";
+        var value = bmi.getBMI(height, hUnit, weight, wUnit);
+
+        expect(value).to.not.be.an.instanceof(Error);
+        expect(value).to.equal(20.99);
+        done();
+    });
+
+    it("Correctly calculates with number strings", function(done) {
+
+        var height = "72";
+        var weight = "150";
+        var hUnit = "inches";
+        var wUnit = "pounds";
+        var value = bmi.getBMI(height, hUnit, weight, wUnit);
+
+        expect(value).to.not.be.an.instanceof(Error);
+        expect(value).to.equal(20.34);
+        done();
+    });
+
+    it("Returns the correct error message with a bad height", function(done) {
+
+        var height = NaN;
+        var weight = "150";
+        var hUnit = "inches";
+        var wUnit = "pounds";
+        var value = bmi.getBMI(height, hUnit, weight, wUnit);
+
+        expect(value).to.be.an.instanceof(Error);
+        expect(value).to.have.property("message").that.equals("Invalid height");
+        done();
+    });
+
+    it("Returns the correct error message with a bad weight", function(done) {
+
+        var height = 72;
+        var weight = "abc";
+        var hUnit = "inches";
+        var wUnit = "pounds";
+        var value = bmi.getBMI(height, hUnit, weight, wUnit);
+
+        expect(value).to.be.an.instanceof(Error);
+        expect(value).to.have.property("message").that.equals("Invalid weight");
+        done();
+    });
+
+    it("Returns an error with an incorrect unit", function(done) {
+
+        var height = 72;
+        var weight = 150;
+        var hUnit = "inches";
+        var wUnit = "wrong unit";
+        var value = bmi.getBMI(height, hUnit, weight, wUnit);
+
+        expect(value).to.be.an.instanceof(Error);
+        done();
+    });
+});
